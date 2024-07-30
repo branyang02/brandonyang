6da12d3aa88be5220a275c0f8bbfa6f4cc7be86f
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2024-07-30 11:46:40 -0400

    revert to generate bold toc after useMemo

--- src/components/NoteToc.tsx
+++ src/components/NoteToc.tsx
@@ -4,31 +4,20 @@ import { useMemo } from 'react';
 import ReactMarkdown from 'react-markdown';
 import rehypeSlug from 'rehype-slug';
 
-const generateTableOfContents = (
-    markdownContent: string,
-    activeId: string,
-): string => {
+const generateTableOfContents = (markdownContent: string) => {
     let res = '# Contents\n---\n';
     const pattern = /^#+ (.*)$/gm;
-    let match;
-
-    while ((match = pattern.exec(markdownContent)) !== null) {
-        const [fullMatch, title] = match;
-        const level = fullMatch.match(/^#+/)?.[0].length || 0;
+    const matches = markdownContent.match(pattern);
+    for (const match of matches || []) {
+        const level = match.match(/^#+/)?.[0].length || 0;
         if (level === 1) continue;
-
-        const cleanedTitle = title.replace(/[*]/g, '').trim();
-        const id = cleanedTitle
+        const title = match.replace(/^#+|\*+/g, '').trim();
+        const id = title
             .replace(/\s+/g, '-')
             .toLowerCase()
             .replace(/[+.()']/g, '');
-
-        const formattedTitle =
-            activeId === id ? `**${cleanedTitle}**` : cleanedTitle;
-
-        res += `${'\t'.repeat(level - 2)}-  [${formattedTitle}](#${id})\n`;
+        res += `${'\t'.repeat(level - 2)}-  [${title}](#${id})\n`;
     }
-
     return res;
 };
 
@@ -40,14 +29,24 @@ const NoteToc = ({
     activeId: string;
 }) => {
     const toc = useMemo(
-        () => generateTableOfContents(markdownContent, activeId),
-        [markdownContent, activeId],
+        () => generateTableOfContents(markdownContent),
+        [markdownContent],
     );
+
+    const tocContent = toc
+        .split('\n')
+        .map((line) => {
+            if (line.includes(`(#${activeId})`)) {
+                return line.replace(/\[(.*?)\]/, '[**$1**]');
+            }
+            return line;
+        })
+        .join('\n');
 
     return (
         <Pane padding="20px" className="note-toc">
             <ReactMarkdown rehypePlugins={[[rehypeSlug, { prefix: 'toc-' }]]}>
-                {toc}
+                {tocContent}
             </ReactMarkdown>
         </Pane>
     );
