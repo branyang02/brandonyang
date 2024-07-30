0a60b07c96a25af3d264dfeba08cecb1f2b6898c
Brandon Yang <jqm9ba@virginia.edu>
2024-07-29 20:49:47 -0400

    updated rollup

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [react()],
     assetsInclude: ['**/*.md'],
+    build: {
+        rollupOptions: {
+            external: ['vfile'],
+        },
+    },
 });
