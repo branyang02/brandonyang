f8e882c24066751532eee833a844a12cb76f666f
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2024-10-02 07:56:11 -0400

    remove logging.

--- src/components/LastUpdated.tsx
+++ src/components/LastUpdated.tsx
@@ -1,6 +1,5 @@
 const LastUpdated = () => {
     const lastUpdatedDate = import.meta.env.VITE_ML_COMMIT_DATE;
-    console.log(lastUpdatedDate);
 
     return (
         <span className="subtitle" style={{ marginBottom: 0, marginTop: 0 }}>
